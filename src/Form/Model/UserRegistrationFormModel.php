06e2b26bbceb4de9f4374e8cf0dd082186cfbbb6
viktorbeznosov <viktorbeznosov@mail.ru>
2021-09-17 23:41:27 +0300

    symfony_diplon start

---
+++ src/Form/Model/UserRegistrationFormModel.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form\Model;
+
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
+
+class UserRegistrationFormModel
+{
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Email()
+     */
+    public $email;
+    public $firstName;
+    /**
+     * @Assert\NotBlank(message="Пароль не указан")
+     * @Assert\Length(min="6", minMessage="Пароль должен быть не менее 6 символов")
+     */
+    public $password;
+
+    public $confirmPassword;
+
+    /**
+     * @Assert\Callback()
+     */
+    public function validate(ExecutionContextInterface $context, $payload)
+    {
+
+        if ($this->confirmPassword != $this->password) {
+            $context->buildViolation('Введенные пароли не совпадают!')
+                ->atPath('confirmPassword')
+                ->addViolation();
+        }
+    }
+
+}
---
+++ src/Form/UserRegistrationFormType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use App\Form\Model\UserRegistrationFormModel;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserRegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName', TextType::class, ['label' => 'Create Task'])
+            ->add('email', EmailType::class, ['label' => 'Ваш email'])
+            ->add('password', PasswordType::class)
+            ->add('confirmPassword', PasswordType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => UserRegistrationFormModel::class
+        ]);
+    }
+}
